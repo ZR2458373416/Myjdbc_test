84d6b7d4d0aba206436cde408b6f4dd37e7743f9
Wxs-070 <as1282732138@163.com>
2020-07-11 17:59:12 +0800

    Initial commit

---
+++ jdbc_test/src/dao/UserOfferPriceDao.java
@@ -0,0 +1,27 @@
+package dao;
+
+import entity.UserOfferPrice;
+import entity.UserProduct;
+import entity.UserTwoTable;
+
+import java.util.List;
+
+public interface UserOfferPriceDao {
+    //查询所有商品
+    List<UserOfferPrice> selectAll(String sql);
+
+    //添加商品
+    int insert(String sql, UserOfferPrice userOfferPrice);
+
+    //删除商品
+    int delete(String sql, UserOfferPrice userOfferPrice);
+
+    //修改商品
+    int update(int prold, UserOfferPrice userOfferPrice, String sql);
+
+    //按商品编号查找
+    List<UserOfferPrice> select(int prold,String sql);
+
+    //查找商品的价格和打折情况
+    List<UserTwoTable> selectAllTwo(String sql);
+}
